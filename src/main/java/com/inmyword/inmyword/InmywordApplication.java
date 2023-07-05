36a6cd6b09418d67850277563a9250afc7c1a914
choizz <tkfka156@gmail.com>
2023-07-05 23:43:45 +0900

    Initial commit

---
+++ src/main/java/com/inmyword/inmyword/InmywordApplication.java
@@ -0,0 +1,13 @@
+package com.inmyword.inmyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmywordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InmywordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inmyword/inmyword/InmywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmyword.inmyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmywordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
